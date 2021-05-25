d6af040ca673d5547f7bfeeae7469e6297e1bd68
ZhangMenghe <zmh.leaves@gmail.com>
2021-05-25 16:45:03 -0700

    ear works on phone but wrong positioned

--- mediapipe/graphs/acu_face/calculators/save_cpu_image_calculator.cc
+++ mediapipe/graphs/acu_face/calculators/save_cpu_image_calculator.cc
@@ -35,11 +35,11 @@ REGISTER_CALCULATOR(SaveCPUImageCalculator);
 }
 
 ::mediapipe::Status SaveCPUImageCalculator::Process(CalculatorContext* cc){
-  if(id %5== 0){
+  // if(id %5== 0){
 
-  const auto& img = cc->Inputs().Index(0).Get<ImageFrame>();
-  cv::imwrite( OUTPUT_PATH + std::to_string(id/5) +".png" , formats::MatView(&img));
-  }
+  // const auto& img = cc->Inputs().Index(0).Get<ImageFrame>();
+  // cv::imwrite( OUTPUT_PATH + std::to_string(id/5) +".png" , formats::MatView(&img));
+  // }
   id++;
   return ::mediapipe::OkStatus();
 }
